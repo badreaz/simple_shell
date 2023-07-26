fb313bb0c4190a9026a7de11583f447c8c1ee795
badreaz <123468928+badreaz@users.noreply.github.com>
2023-07-26 23:17:40 +0400

    Update operators.c

--- operators.c
+++ operators.c
@@ -2,30 +2,24 @@
 
 /**
  * operators - handle command aand logical operators
- * @line: command line.
+ * @commands: command line.
  *
  * Return: 1 if there is no operators, otherwise 0..
  */
-int operators(char *line)
+int operators(node_t *commands)
 {
 	char *start;
-	node_t *commands;
 	static int n = 1;
 
 	if (n != 1)
 		n++;
-	commands = malloc(sizeof(node_t));
-	commands->next = malloc(sizeof(node_t));
-	commands->str = line;
-	commands->next->str = start = line;
-	commands->next->next = NULL;
+	commands->next->str = start = commands->str;
 	while (*start != '\n')
 	{
 		if (_strncmp(start, " #", 2) == 0)
 		{
 			start[0] = '\0';
 			execute(commands, n);
-			freenode(commands);
 			return (0);
 		}
 		else if (_strncmp(start, ";", 1) == 0)
@@ -38,25 +32,18 @@ int operators(char *line)
 		{
 			start[0] = '\0';
 			if (execute(commands, n))
-			{
-				freenode(commands);
 				return (0);
-			}
 			commands->next->str = start + 2;
 		}
 		else if (_strncmp(start, "||", 2) == 0)
 		{
 			start[0] = '\0';
 			if (execute(commands, n) == 0)
-			{
-				freenode(commands);
 				return (0);
-			}
 			commands->next->str = start + 2;
 		}
 		start++;
 	}
 	execute(commands, n);
-	freenode(commands);
 	return (1);
 }
