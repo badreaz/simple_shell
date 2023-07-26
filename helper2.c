c799ba902e8be7bd0ac748cb584b56b127c7a11f
badreaz <123468928+badreaz@users.noreply.github.com>
2023-07-27 01:22:32 +0400

    Update helper2.c

--- helper2.c
+++ helper2.c
@@ -26,3 +26,19 @@ int _atoi(char *s)
 
 	return (num * m);
 }
+
+/**
+ * _isdigit - check if the character is a digit.
+ * @c: number in ascii code.
+ *
+ * Return:
+ * - if true 1.
+ * - otherwise 0.
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	else
+		return (0);
+}
