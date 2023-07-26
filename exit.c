9164d3d27722e43a9dcf412cea3034116814a427
badreaz <123468928+badreaz@users.noreply.github.com>
2023-07-26 22:50:25 +0400

    Create exit.c

---
+++ exit.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * myexit - exit the program
+ * @line: command line.
+ * @mynode: commands and arguments.
+ * @n: line number.
+ */
+void myexit(node_t *line, node_t *mynode, int n)
+{
+  char exit_n = "exit";
+  int i = -1;
+
+  if (_strcmp(mynode->str, exit_n) == 0)
+	{
+    if (mynode->next)
+    {
+      i = _atoi(mynode->next->str);
+      if (i < 0)
+      {
+        _perror(n, mynode->str);
+        write(1, ": ", 2);
+        write(1, nynode->next->str, 10);
+      }
+      else
+        errno = i;
+    }
+		freenode(line);
+		freenode(mynode);
+		exit(errno);
+	}
+}
