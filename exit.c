a09acc4ca87bf04844ab07ca1f92dcc87d9f745e
badreaz <123468928+badreaz@users.noreply.github.com>
2023-07-26 22:53:55 +0400

    Update exit.c

--- exit.c
+++ exit.c
@@ -8,7 +8,7 @@
  */
 void myexit(node_t *line, node_t *mynode, int n)
 {
-  char exit_n = "exit";
+  char *exit_n = "exit";
   int i = -1;
 
   if (_strcmp(mynode->str, exit_n) == 0)
@@ -20,7 +20,7 @@ void myexit(node_t *line, node_t *mynode, int n)
       {
         _perror(n, mynode->str);
         write(1, ": ", 2);
-        write(1, nynode->next->str, 10);
+        write(1, mynode->next->str, 10);
       }
       else
         errno = i;
