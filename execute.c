068c047350297b8806f77cd32ed8d0ac81ab830b
badreaz <123468928+badreaz@users.noreply.github.com>
2023-07-26 20:57:27 +0400

    Update execute.c

--- execute.c
+++ execute.c
@@ -58,7 +58,7 @@ int execute(node_t *line, int n)
 	if (WIFEXITED(status))
 	{
 		errno = WEXITSTATUS(status);
-		printf("%d\n", errno);
+		/*printf("%d\n", errno);*/
 		return (-1);
 	}
 	return (0);
